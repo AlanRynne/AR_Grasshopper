74c558fc27dc0ebfb02a17075eb9a5149f10044c
Alan Rynne <alan@rynne.es>
2019-03-17 19:49:18 +0100

    Added  working VEF component

--- AR_Grasshopper/MeshTopology/MeshTopologyVEFComponent.cs
+++ AR_Grasshopper/MeshTopology/MeshTopologyVEFComponent.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using AR_Lib.HalfEdgeMesh;
 using Grasshopper.Kernel;
 using Rhino.Geometry;
 
@@ -42,6 +42,55 @@ namespace AR_Grasshopper.MeshTopology
         /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            HE_MeshGHData hE_MeshData = new HE_MeshGHData();
+
+            if (!DA.GetData(0, ref hE_MeshData)) return;
+
+            HE_Mesh hE_Mesh = hE_MeshData.Value;
+
+            List<Point3d> vertices = new List<Point3d>();
+            List<Line> edges = new List<Line>();
+            List<Mesh> faces = new List<Mesh>();
+
+            foreach(HE_Vertex v in hE_Mesh.Vertices)
+            {
+                vertices.Add(new Point3d(v.X, v.Y, v.Z));
+            }
+            foreach (HE_Edge e in hE_Mesh.Edges)
+            {
+                HE_Vertex v1 = e.HalfEdge.Vertex;
+                HE_Vertex v2 = e.HalfEdge.Twin.Vertex;
+
+                edges.Add(new Line(new Point3d(v1.X, v1.Y, v1.Z), new Point3d(v2.X, v2.Y, v2.Z)));
+            }
+            foreach (HE_Face f in  hE_Mesh.Faces)
+            {
+                List<HE_Vertex> vs = f.adjacentVertices();
+            
+                List<int> faceVs = new List<int>();
+                List<Point3d> facePoints = new List<Point3d>();
+
+                int vi = 0;
+
+                foreach(HE_Vertex v in vs)
+                {
+                    facePoints.Add(new Point3d(v.X, v.Y, v.Z));
+                    faceVs.Add(vi);
+                    vi++;
+                }
+
+                Mesh m = new Mesh();
+                m.Vertices.AddVertices(facePoints);
+
+                if (vs.Count == 3) m.Faces.AddFace(0, 1, 2);
+                else if (vs.Count == 4) m.Faces.AddFace(0, 1, 2, 3);
+
+                faces.Add(m);
+            }
+
+            DA.SetDataList(0, vertices);
+            DA.SetDataList(1, edges);
+            DA.SetDataList(2, faces);
         }
 
         /// <summary>
@@ -64,5 +113,5 @@ namespace AR_Grasshopper.MeshTopology
         {
             get { return new Guid("b72a840e-ee01-4292-bb39-149e830fd3ab"); }
         }
-    }
+    }  
 }
